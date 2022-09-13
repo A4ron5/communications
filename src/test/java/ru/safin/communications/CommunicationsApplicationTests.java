dacda3c6087d170bac035ae7133d9cfeffba86a7
Maksim D Safin <crockolaco@yandex.ru>
2022-09-13 21:05:12 +0500

    init

---
+++ src/test/java/ru/safin/communications/CommunicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.safin.communications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
