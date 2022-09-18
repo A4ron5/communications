19214b5934adeff0b8e2114924d8490d44da273e
Maksim D Safin <crockolaco@yandex.ru>
2022-09-19 00:02:05 +0500

    feat: group controller valid path

--- src/main/java/ru/safin/communications/groups/GroupsRestController.java
+++ src/main/java/ru/safin/communications/groups/GroupsRestController.java
@@ -1,12 +1,15 @@
 package ru.safin.communications.groups;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.safin.communications.groups.dto.GroupDto;
 import ru.safin.communications.groups.models.entities.Group;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
+@Validated
 @RestController
 @RequestMapping("/api/groups")
 public class GroupsRestController {
@@ -17,12 +20,12 @@ public class GroupsRestController {
   }
 
   @PutMapping ("/add/{id}")
-  public ResponseEntity<Group> addToGroup(@PathVariable String id, @RequestBody GroupDto groupDto) {
+  public ResponseEntity<Group> addToGroup(@NotBlank @PathVariable String id, @RequestBody GroupDto groupDto) {
     return null;
   }
 
   @DeleteMapping("delete/{id}")
-  public ResponseEntity<Group> deleteFromGroup(@PathVariable String id, @RequestBody GroupDto groupDto) {
+  public ResponseEntity<Group> deleteFromGroup(@NotBlank @PathVariable String id, @RequestBody GroupDto groupDto) {
     return null;
   }
 }
